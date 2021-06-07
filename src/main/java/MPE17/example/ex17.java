e390781aee0c86f9c4100c1f917ba315942c88dd
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-06 23:59:03 -0400

    Initial commit

---
+++ src/main/java/MPE17/example/ex17.java
@@ -0,0 +1,19 @@
+package MPE17.example;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Derek Gilberti
+ */
+
+//class begin
+public class ex17 {
+    //main method begin
+    public static void main(String[] args) {
+        //scanner
+        Scanner in = new Scanner(System.in);
+
+        //input
+    }
+}
